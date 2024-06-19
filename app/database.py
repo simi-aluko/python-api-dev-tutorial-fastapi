58142667eae241b531860a43128d454164acdd7c
Simileoluwa Aluko (GitHub) <simi@yelp.com>
2024-06-19 19:56:34 +0100

    changed hello world

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 import time
 
-import psycopg2
-from psycopg2.extras import RealDictCursor
+# import psycopg2
+# from psycopg2.extras import RealDictCursor
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!!!!!"}
 
 
 
